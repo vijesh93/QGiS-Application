f797e24dbd62713422886df6815121adb0b8b96e
vijesh <vijesh.vidhani93@gmail.com>
2026-02-13 11:06:40 +0100

    DB password management bug fixed

--- backend/db.py
+++ backend/db.py
@@ -1,9 +1,24 @@
 from sqlmodel import Session, create_engine
 import os
+from urllib.parse import quote_plus
+
 
 # Central DB engine and session provider used across the backend
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://admin:password@db:5432/geoportal")
+user_name = os.getenv("DB_USER")
+encoded_password = os.getenv("DB_PASSWORD_URL")
+
+# Try to get the full URL from env, otherwise build it using f-string
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    DATABASE_URL = f"postgresql://{user_name}:{encoded_password}@db:5432/geoportal"
+
+else:
+    # TODO: Figure out what is better way to enter a password for DB.
+    raise NotImplementedError
+
+# print(f"Connecting to: {DATABASE_URL.replace(encoded_password, '****')}") # Safety print
 engine = create_engine(DATABASE_URL)
+
 
 def get_session():
     with Session(engine) as session:
